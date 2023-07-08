95947fa0ba0b7769c32f9114a0cac8a4e2ca0474
thcheetah777 <thcheetah777@gmail.com>
2023-07-08 11:15:07 +0200

    Cheeze movement
    The cheeze now has 3 states: far, medium, and close

--- Assets/Scripts/Cheese.cs
+++ Assets/Scripts/Cheese.cs
@@ -1,8 +1,56 @@
 using UnityEngine;
 
+enum CheeseState
+{
+    Far,
+    Medium,
+    Close
+}
+
 public class Cheese : MonoBehaviour
 {
 
-    
+    [SerializeField] private LayerMask ratLayer;
+
+    [Header("State")]
+
+    [SerializeField] private float _farRange;
+    [SerializeField] private float _mediumRange;
+    [SerializeField] private float _closeRange;
+
+    [SerializeField] private float _farSpeed;
+    [SerializeField] private float _mediumSpeed;
+    [SerializeField] private float _closeSpeed;
+
+    private CheeseState _state;
+
+    void Update() {
+        _state = CheeseState.Close;
+
+        float distanceFromRat = Vector2.Distance(transform.position, HordeController.Rats[0].transform.position);
+        if (distanceFromRat >= _mediumRange) _state = CheeseState.Medium;
+        if (distanceFromRat >= _farRange) _state = CheeseState.Far;
+
+        switch (_state)
+        {
+            case CheeseState.Far: {
+                Move(_farSpeed);
+                break;
+            }
+            case CheeseState.Medium: {
+                Move(_mediumSpeed);
+                break;
+            }
+            case CheeseState.Close: {
+                Move(_closeSpeed);
+                break;
+            }
+        }
+    }
+
+    private void Move(float speed) {
+        Vector2 direction = HordeController.Rats[0].transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, direction, speed * Time.deltaTime);
+    }
 
 }
